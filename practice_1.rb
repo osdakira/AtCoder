fd90b28246852037a09968af894325f627af1105
長田晃 <osada@osadaakira-no-MacBook-Air.local>
2024-11-09 20:33:21 +0900

    ruby test

--- practice_1.rb
+++ practice_1.rb
@@ -1,4 +1,34 @@
-a = gets.to_i
-b, c = gets.chomp.split(" ").map(&:to_i)
-s = gets.chomp
-puts("#{a + b + c} #{s}")
+# a = gets.to_i
+# b, c = gets.chomp.split(" ").map(&:to_i)
+# s = gets.chomp
+# puts("#{a + b + c} #{s}")
+
+class Meme
+  def i_can_has_cheezburger?
+    "OHAI!"
+  end
+
+  def will_it_blend?
+    "YES!"
+  end
+end
+
+require "minitest/autorun"
+
+describe Meme do
+  before do
+    @meme = Meme.new
+  end
+
+  describe "when asked about cheeseburgers" do
+    it "must respond positively" do
+      _(@meme.i_can_has_cheezburger?).must_equal "OHAI!"
+    end
+  end
+
+  describe "when asked about blending possibilities" do
+    it "won't say no" do
+      _(@meme.will_it_blend?).wont_match /^no/i
+    end
+  end
+end
